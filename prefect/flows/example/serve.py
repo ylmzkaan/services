1897ff8bfbc4d4f49bb02852f50abdb2cd0c8e53
Kaan Yilmaz <kaan-yilmaz@outlook.com.tr>
2024-04-28 01:42:29 +0200

    Fix flow server path

--- prefect/flows/example/serve.py
+++ prefect/flows/example/serve.py
@@ -2,6 +2,6 @@ from prefect import flow
 
 if __name__ == "__main__":
     flow.from_source(
-        source="https://github.com/ylmzkaan/prefectflows.git",
-        entrypoint="toy_flow.py:say_hi",
+        source="https://github.com/ylmzkaan/services.git",
+        entrypoint="prefect/flows/example/flow.py:say_hi",
     ).serve(name="deployment-with-github-storage")
