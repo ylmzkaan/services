925d4ee3c26625f4d8ab9bd9f9f95351dee4e300
Kaan Yilmaz <kaanyilmaz.se@gmail.com>
2024-04-28 20:43:42 +0200

    Add deploy scripts for simple app

---
+++ simple-app/deploy_k8s.sh
@@ -0,0 +1,2 @@
+kubectl create namespace simple-app
+kubectl apply -f resources.yaml -n simple-app
---
+++ simple-app/port_forward.sh
@@ -0,0 +1,1 @@
+kubectl --namespace simple-app port-forward svc/simple-app 4203:4203 --address=0.0.0.0
